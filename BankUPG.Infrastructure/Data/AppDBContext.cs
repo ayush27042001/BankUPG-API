9b0659b27e1ee1bc426a975eef5c16e91409b860
Ayush Awasthi <ayushawasthi731@gmail.com>
2026-06-25 22:30:28 +0530

    Updated Connection String

--- BankUPG.Infrastructure/Data/AppDBContext.cs
+++ BankUPG.Infrastructure/Data/AppDBContext.cs
@@ -69,7 +69,7 @@ public partial class AppDBContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=103.205.142.34,1433;Initial Catalog=BankuPG;Persist Security Info=True;User ID=sa;Password=zUG93NOh6WE7BQIS;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=97.74.80.235,1232;Initial Catalog=BankuPG;Persist Security Info=True;User ID=sa;Password=BankU@2022*1std^ca;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
